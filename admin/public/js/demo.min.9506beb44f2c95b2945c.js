6691fc68c62b4a9f4bc6ca080001cb96f4990962
tier5 <github@tier5.us>
2016-03-30 18:23:03 +0530

    image path fixed

--- admin/public/js/demo.min.9506beb44f2c95b2945c.js
+++ admin/public/js/demo.min.9506beb44f2c95b2945c.js
@@ -6,17 +6,16 @@
 function() {
     "use strict";
    
-
     function a(a,file_path) {
         a.when("/", {
-            templateUrl: "../../view/dashboard.html"
+            templateUrl: "../view/dashboard.html"
         }).when("/:folder/:tpl", {
             templateUrl: function(a) {
-                return "../../view/" + a.folder + "/" + a.tpl + ".html"
+                return "../view/" + a.folder + "/" + a.tpl + ".html"
             }
         }).when("/:tpl", {
             templateUrl: function(a) {
-                return "../../view/" + a.tpl + ".html"
+                return "../view/" + a.tpl + ".html"
             }
         }).otherwise({
             redirectTo: "/"
@@ -62,10 +61,13 @@ function() {
             h.get("../admin/dashboard/userdetails")
             .success(function (data) {                
                 a.ReedemerDetails=data;
-            });           
+            });   
+          
+          // set path ../ for local and ../../ for server
+          a.d_path="../../";  
         } 
 
-
+        
        
         angular.module("material-lite").controller("MainController", ["$scope","$http", a])
     }(),
@@ -617,7 +619,10 @@ function() {
                 a.cnames = response;
             
           })
+
+
         }
+
         angular.module("material-lite").controller("ReedemerController", ["$scope", "PlaceholderTextService", "ngTableParams", "$filter", "$http", "fileUpload", a])
     }(),    
     function() {
@@ -716,7 +721,7 @@ function() {
         function a() {
             return {
                 restrict: "E",
-                templateUrl: "../../view/partials/header.html",
+                templateUrl: "../view/partials/header.html",
                 replace: !0
             }
         }
@@ -728,7 +733,7 @@ function() {
         function a() {
             return {
                 restrict: "E",
-                templateUrl: "../../view/partials/sidebar.html",
+                templateUrl: "../view/partials/sidebar.html",
                 replace: !0
             }
         }
@@ -861,7 +866,7 @@ function() {
                 restrict: "EA",
                 transclude: !0,
                 replace: !0,
-                templateUrl: "../../view/tpl/partials/menu-item.html",
+                templateUrl: "../view/tpl/partials/menu-item.html",
                 scope: {
                     isActive: "=?"
                 },
@@ -882,7 +887,7 @@ function() {
                 restrict: "EA",
                 transclude: !0,
                 replace: !0,
-                templateUrl: "../../view/tpl/partials/menu-group.html",
+                templateUrl: "../view/tpl/partials/menu-group.html",
                 scope: {
                     heading: "@",
                     path: "@",
@@ -1086,7 +1091,7 @@ function() {
             }
             return {
                 restrict: "EA",
-                templateUrl: "../../view/tpl/partials/todo-widget.html",
+                templateUrl: "../view/tpl/partials/todo-widget.html",
                 replace: !0,
                 link: b
             }
