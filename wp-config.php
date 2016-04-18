cfaec40f60b4b21e83c8eebaf0dbdc7aeacfc6e3
Jon V <jon@tier5.us>
2016-04-18 16:55:07 +0530

    changes

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'reedemer');
+define('DB_NAME', 'reedmer');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'P@ssW0rd');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
