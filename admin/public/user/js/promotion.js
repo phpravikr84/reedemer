a31785eddb816f48c2e580bd17c0917caf7d5f6e
tier5 <github@tier5.us>
2016-05-19 15:38:00 +0530

    working on offer builder

--- admin/public/user/js/promotion.js
+++ admin/public/user/js/promotion.js
@@ -34,54 +34,38 @@ MyApp.factory("fileToUpload", ["$http", function(h){
 
 
 MyApp.controller('PromotionController',["$scope", "PlaceholderTextService", "ngTableParams", "$filter", "$http", "fileToUpload",function (a, b, c, d, x, fu) {          
-    //a.dataLength={filtered:[]};
-    //a.cnames = [];
-    //a.inventory_details = [];   
     var site_path=$("#site_path").val();
 
-     $( "#c_s_date" ).datepicker({         
-            dateFormat:"mm/dd/yy",
-           // minDate:response_details[0].start_date,
-            onSelect: function (selected) {
-              var dt = new Date(selected);
-              dt.setDate(dt.getDate() + 1);
-              $("#c_e_date").datepicker("option", "minDate", dt);
-            }
+    $( "#c_s_date" ).datepicker({         
+      dateFormat:"mm/dd/yy",          
+      onSelect: function (selected) {
+        var dt = new Date(selected);
+        dt.setDate(dt.getDate() + 1);
+        $("#c_e_date").datepicker("option", "minDate", dt);
+      }
 
-          });
+    });
 
-          $( "#c_e_date" ).datepicker({
-            dateFormat:"mm/dd/yy",
-            onSelect: function (selected) {
-              var dt = new Date(selected);
-              dt.setDate(dt.getDate() - 1);
-              $("#c_s_date").datepicker("option", "maxDate", dt);
-            }
-          });
-    
+    $( "#c_e_date" ).datepicker({
+      dateFormat:"mm/dd/yy",
+      onSelect: function (selected) {
+        var dt = new Date(selected);
+        dt.setDate(dt.getDate() - 1);
+        $("#c_s_date").datepicker("option", "maxDate", dt);
+      }
+    });    
    
-    x.post("../campaign/list").success(function(response){              
-      //alert(JSON.stringify(response,null,4));
+    x.post("../campaign/list").success(function(response){ 
       a.campaign_list = response; 
-      a.file_path=site_path;  
-      //a.PromotionDetails.desC = "Winter vegetable pasta with white wine and";
+      a.file_path=site_path;       
     }); 
 
-    x.get("../admin/dashboard/category").success(function(response_cat){              
-      //alert(JSON.stringify(response_cat,null,4));
-      a.cat_list = response_cat; 
-     // a.file_path=site_path;  
+    x.get("../admin/dashboard/category").success(function(response_cat){ 
+      a.cat_list = response_cat;        
     }); 
 
-    x.post("../inventory/list").success(function(inventory_list){        
-     // alert(JSON.stringify(inventory_list[0].id,null,4));
-      a.inventory_list = inventory_list;   
-      //$("#inventory_list").val($("#inventory_list option:first").val());
-
-      //$('#inventory_list option:first-child').attr("selected", "selected");
-      //a.inventory_id = a.inventory_list[0];
-      
- 
+    x.post("../inventory/list").success(function(inventory_list){ 
+      a.inventory_list = inventory_list;  
     }); 
 
     a.open_pop = function(item){  
@@ -104,165 +88,104 @@ MyApp.controller('PromotionController',["$scope", "PlaceholderTextService", "ngT
             width: 400,
             title: 'Dynamically Loaded Page'
         });
-  }
+    }
 
-  a.choices = [{id: '1'}];
-  a.addNewChoice = function() {
-    var newItemNo = a.choices.length+1;
-    a.choices.push({'id':newItemNo});
-  };
-    
-  a.removeChoice = function() {
-    var lastItem = a.choices.length-1;
-    a.choices.splice(lastItem);
-  };
-   // $("#inventory_list option:last").attr("selected", true);
-
-   
-    
-    //$("select#inventory_list").val('3')
-   // alert("a");
-    //alert($("#inventory_list").val());
-
-
-   // x.post("../inventory/list").success(function(data_response){              
-     // a.inventory_details = data_response;   
-
-    //  a.ProductList = data_response;
-
-      // a.ProductList = null;
-      // //Declaring the function to load data from database
-      // a.fillProductList = function () {
-      //     x({
-      //         method: 'POST',
-      //         url: '../inventory/list',
-      //         data: {}
-      //     }).success(function (result) {
-      //         a.ProductList = result.d;
-      //         alert(JSON.stringify(a.ProductList,null,4));
-      //     });
-      // };
-      // //Calling the function to load the data on pageload
-      // a.fillProductList();
-
-
-   // }); 
- // alert(JSON.stringify(inventory_list,null,4));
-  // a.choices = [{id: 'choice1'}];
-  // a.addNewChoice = function() {
-  //   var newItemNo = a.choices.length+1;
-  //   a.data_length=newItemNo;
-  //   a.choices.push({'id':'choice'+newItemNo});
-  // };
-    
-  // a.removeChoice = function() {
-  //   var lastItem = a.choices.length-1;
-  //   a.choices.splice(lastItem);
-  // };
-
-    
-        
-   // a.pup = function()
-   //      {
-   //        alert("N");
-   //      }
-
- 
-
-
-
-  
-    a.get_inventory = function(item, choices){  
-     
-      //inventory_id={}   ;
-
-      //alert(a.inventory_id);
-      console.log("item :: "+item+" choice :: "+JSON.stringify(choices, null, 4));
-      // var inventory_id=$("#inventory_list").val();
-      var inventory_id=item;
-     // if (!angular.isUndefined(a.inventory_id)) {
-       // alert(newItemNo);
-       // var inventory_id=a.inventory_id.id;
-       // var inventory_id=$("#inventory_list").val();
-
-       // alert(inventory_id);
-
-        x.post("../inventory/inventorydetails",inventory_id).success(function(data_item){ 
-          //alert(JSON.stringify(data_item,null,4));
-          a.inventory_item=data_item;
-          a.file_path=site_path;
-         // alert("B");
-         //alert(JSON.stringify(data_item,null,4));
-         var costId = "#cost"+choices;
-         $(costId).val(data_item.cost);
-         var imageDivId = "#selling_price"+choices;
-         $(imageDivId).val(data_item.sell_price);
-         var src=site_path+"../uploads/inventory/original/"+data_item.inventory_image;
-         var inventoryImageId = "#inventory_image"+choices;
-         $((inventoryImageId)).attr("src", src);
-         // a.inventory_item.inventory_cost=data_response.cost;
-         // a.inventory_item.inventory_sell_price=data_response.sell_price;
-
-        }); 
-
-     // }
-      //else
-      //{
-       // alert("a");
-     // }
-      //alert(JSON.stringify(angular.isDefined(a.inventory_id),null,4));
+    a.choices = [{id: '1'}];
+    a.addNewChoice = function() {
+      var newItemNo = a.choices.length+1;
+      a.choices.push({'id':newItemNo});
+    };
       
-    } 
+    a.removeChoice = function() {
+      var lastItem = a.choices.length-1;
+      a.choices.splice(lastItem);
+    };
 
-    //a.result = a.someVal;
+    a.get_inventory = function(item, choices){
+      var inventory_id=item;     
+      x.post("../inventory/inventorydetails",inventory_id).success(function(data_item){        
+        a.inventory_item=data_item;
+        a.file_path=site_path;        
+        var costId = "#cost"+choices;
+        $(costId).val(data_item.cost);
+        var imageDivId = "#selling_price"+choices;
+        $(imageDivId).val(data_item.sell_price);
+        var src=site_path+"../uploads/inventory/original/"+data_item.inventory_image;
+        var inventoryImageId = "#inventory_image"+choices;
+        $((inventoryImageId)).attr("src", src);        
+      }); 
+    }   
+
     a.offer_description = "";
     a.what_you_get = "";
     a.cost = "";
 
-    $("#include_product_value").click(function() {
-        // this function will get executed every time the #home element is clicked (or tab-spacebar changed)
-        if($(this).is(":checked")) // "this" refers to the element that fired the event
+ 
+    a.include_price=function()
+    {      
+      // this function will get executed every time the #home element is clicked (or tab-spacebar changed)
+      if($("#include_product_value").is(":checked")) // "this" refers to the element that fired the event
+      {
+        //var selling_price_class_items = $('.selling_price_class').length;
+        
+        // Sum Retails value price
+        var total = 0;
+        var $changeInputs = $('input.selling_price_class');
+        $changeInputs.each(function(idx, el) 
         {
-            var total_cost=$("#cost").val();
-            var selling_price=$("#selling_price").val();
-            //alert('home is checked');
-            $("#pay_value").val(total_cost);
-            $("#retails_value").val(selling_price);
-        }
-    });
-   
-   // a.desC = 'bob';
-    //a.PromotionDetails="bbbbbb";
-    
-
-    // $("input[type='button']").click(function(){
-    // var radioValue = $("input[name='gender']:checked").val();
-    // if(radioValue){
-    // alert("Your are a - " + radioValue);
-    // }
-    // });     
-
-   
-    // Change total price value with no of redeemar change
-    $( '#total_redeemar' ).keyup(function() {       
-       var selectd_val=$('input[name="total_payment"]:checked').val();       
-       var total_price=selectd_val*$("#total_redeemar").val();
-              
-       $("#total_redeemar_price").val(total_price);
-    });   
-
-    // Change total price value with radio button click
-    $('input[name="total_payment"]').on('change', function() {
-       var selectd_val=$('input[name="total_payment"]:checked').val();        
-       var total_price=selectd_val*$("#total_redeemar").val();      
-       
-       $("#total_redeemar_price").val(total_price);
-    });       
-  
-    a.add_offer=function(){  
-      alert(JSON.stringify(a.promotion, null, 4));
+          total += Number($(el).val());
+        });
+        $('#retails_value').val(total);
+        //$('#pay_value').val(total);
+        
+      }
     }
 
+    a.set_discount=function()
+    {
+      //alert("b");
+      var retails_value=$("#retails_value").val();
+      var pay_value=$("#pay_value").val();
 
+      var discount=parseFloat(retails_value)-parseFloat(pay_value);
+
+      var value_calculate=$("input[name=value_calculate]:checked").val();
+
+      if(value_calculate==2 || value_calculate==4 || value_calculate==6)
+      {
+        var total_discount=discount.toFixed(2);
+        var total_discount_show='$'+total_discount;
+      }
+      if(value_calculate==1 || value_calculate==3 || value_calculate==5)
+      {
+        var total_discount=((discount/retails_value)*100).toFixed(2);
+        var total_discount_show=total_discount+'%';
+      }
+      $("#off_value").val(total_discount);
+      $("#discount_value").val(total_discount);
+      $("#saving_value").val(total_discount);
+      $("#save_value_show").html(total_discount_show);
+
+     // alert(discount);
+    }
+ 
+   
+  // Change total price value with no of redeemar change
+  $( '#total_redeemar' ).keyup(function() {       
+     var selectd_val=$('input[name="total_payment"]:checked').val();       
+     var total_price=selectd_val*$("#total_redeemar").val();
+            
+     $("#total_redeemar_price").val(total_price);
+  });   
+
+  // Change total price value with radio button click
+  $('input[name="total_payment"]').on('change', function() {
+     var selectd_val=$('input[name="total_payment"]:checked').val();        
+     var total_price=selectd_val*$("#total_redeemar").val();      
+     
+     $("#total_redeemar_price").val(total_price);
+  });       
+
+  
 
 }]);
